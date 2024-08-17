d16ef60472f74a3d1d5e7fc62d2a13fbd8443d0a
Jonathan A <jonathanxl1@hotmail.com>
2024-08-17 10:09:44 -0500

    feat: Add layout to manage pages Nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <nav>
+      <NavBar />
+    </nav>
+    <main>
+      <NuxtPage class="w-full h-auto bg-gray-100 p-5 font-sans"></NuxtPage>
+    </main>
+    <FooterBar />
+  </div>
+</template>
