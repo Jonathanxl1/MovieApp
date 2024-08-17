ad4a7dadebe9e0a8f57b11a7a356b90a73b3198c
Jonathan A <jonathanxl1@hotmail.com>
2024-08-17 17:09:49 -0500

    Refactor:Desocuple SearchBar like component resource

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -5,15 +5,7 @@
         <Image class="w-auto h-auto" src="./img/logo.png" />
       </div>
     </NuxtLink>
-
-    <AutoComplete
-      class="w-3/4"
-      unstyled
-      placeholder="Busca peliculas o Series ..."
-      inputClass="w-full h-10 rounded-lg p-2 focus:border-yellow-400 focus:border-4 "
-      v-model="search"
-      fluid>
-    </AutoComplete>
+    <SearchBar />
     <Button
       label="login"
       link
@@ -21,26 +13,3 @@
       class="text-white hover:text-yellow-400 hover:cursor-pointer hover:underline" />
   </nav>
 </template>
-<script>
-  export default {
-    data() {
-      return {
-        search: "",
-        debounce: null,
-      };
-    },
-    methods: {
-      searchMovie: useSearch(),
-    },
-    watch: {
-      search(newValue) {
-        if (this.debounce) {
-          clearTimeout(debounce);
-        }
-        this.debounce = setTimeout(() => {
-          this.searchMovie(newValue);
-        }, 800);
-      },
-    },
-  };
-</script>
