83c5b862fc53c1b5023e9d2f7bf6e9a2fd125e20
Jonathan A <jonathanxl1@hotmail.com>
2024-08-17 07:48:50 -0500

    style:Define Style keep PrimeVue

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+import Aura from "@primevue/themes/aura";
+
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: false },
+  modules: ["@primevue/nuxt-module", "@nuxtjs/tailwindcss"],
+  primevue: {
+    options: {
+      theme: Aura,
+      ripple: true,
+    },
+    autoImport: true,
+  },
+});
