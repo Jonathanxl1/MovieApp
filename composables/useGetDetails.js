56c46223534b57cf0bc6ebd4a3bc4f7962086fb7
Jonathan A <jonathanxl1@hotmail.com>
2024-08-19 19:15:26 -0500

    fix: Solve getDetails movie

--- composables/useGetDetails.js
+++ composables/useGetDetails.js
@@ -3,6 +3,6 @@ import { baseApi } from "~/utils/api";
 export default function () {
   return function (id) {
     if (!id) return;
-    return baseApi(id, { query: string });
+    return baseApi(`movie/${id}`, {}, true);
   };
 }
