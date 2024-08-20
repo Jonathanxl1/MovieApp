6aeb8f8e7a3efc3c5fc5bf655c6c56045602d67f
Jonathan A <jonathanxl1@hotmail.com>
2024-08-19 19:22:41 -0500

    feat:Added manage loading state page

--- middleware/01.index.global.js
+++ middleware/01.index.global.js
@@ -1,2 +1,7 @@
 export default defineNuxtRouteMiddleware((to, from) => {
+  const stateUi = useStateUi();
+  stateUi.setLoading(true);
+  setTimeout(() => {
+    stateUi.setLoading(false);
+  }, 2000);
 });
