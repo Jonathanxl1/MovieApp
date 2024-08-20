ba07700be2528ee2ccaf380c0dada052841ca651
Jonathan A <jonathanxl1@hotmail.com>
2024-08-20 13:58:32 -0500

    style: Adjust design mobile device
    - Adde realese date

--- pages/[id].vue
+++ pages/[id].vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="w-full h-8/12 columns-2 flex px-10">
-    <div class="w-2/4 h-full aspect-square">
+  <div
+    class="sm:w-full sm:h-screen sm:flex sm:flex-col sm:items-center sm:columns-1 lg:w-full lg:h-8/12 lg:columns-2 lg:flex lg:flex-row px-10">
+    <div class="lg:w-2/4 lg:h-full lg:aspect-square">
       <!-- <PosterView srcPath=""> -->
       <Image
         :src="getSrcImage(detailsMovie.poster_path)"
         imageClass="h-full " />
     </div>
 
-    <div class="w-2/4 flex flex-col space-y-3">
+    <div class="sm:flex sm:justify-start lg:w-2/4 flex-col lg:space-y-3 h-full">
       <div>
-        <h1>{{ detailsMovie.original_title }}</h1>
+        <h1 class="text-2xl">{{ detailsMovie.original_title }}</h1>
       </div>
       <div>
-        <i class="pi pi-star-fill"></i>
-        Rating: {{ detailsMovie.vote_average }}
+        <i class="pi pi-star-fill text-yellow-400"></i>
+        <span class="font-bold">Rating:</span> {{ detailsMovie.vote_average }}
       </div>
-      <div></div>
+      <div>
+        <span class="font-bold"> Realese :</span>
+        {{ detailsMovie.release_date }}
+      </div>
       <div>{{ detailsMovie.overview }}</div>
     </div>
     <div></div>
