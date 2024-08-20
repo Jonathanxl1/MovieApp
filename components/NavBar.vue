0ea54a2f03a63d38550e8d900419f326c1d610f9
Jonathan A <jonathanxl1@hotmail.com>
2024-08-20 02:03:22 -0500

    fix: Solve call resource when load logo on page Seach

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,7 +2,7 @@
   <nav class="h-15 w-full bg-slate-800 py-3 px-1 flex items-center space-x-5">
     <NuxtLink to="/">
       <div class="container w-20 h-20">
-        <Image class="w-auto h-auto" src="./img/logo.png" />
+        <Image class="w-auto h-auto" src="/img/logo.png" />
       </div>
     </NuxtLink>
     <SearchBar />
