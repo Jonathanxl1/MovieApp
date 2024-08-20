0b1eb87199f20b3e98f58259668d6a5449c0b016
Jonathan A <jonathanxl1@hotmail.com>
2024-08-20 04:01:27 -0500

    style:Adjust design

--- components/FooterBar.vue
+++ components/FooterBar.vue
@@ -1,5 +1,6 @@
 <template>
-  <footer class="w-full bg-black h-40 absolute mt-auto">
-    <p class="text-white">Footer</p>
+  <footer
+    class="w-full bg-black h-32 absolute mt-auto flex items-center justify-center">
+    <p class="text-white">Made by Kubo</p>
   </footer>
 </template>
