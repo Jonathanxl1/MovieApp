2647f98d2d00d266fbcea3b8c473fff9486a7d31
Jonathan A <jonathanxl1@hotmail.com>
2024-08-20 03:59:13 -0500

    style:Adding link design realistic

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,5 +1,6 @@
 <template>
-  <nav class="h-15 w-full bg-slate-800 py-3 px-1 flex items-center space-x-5">
+  <nav
+    class="h-15 w-screen bg-slate-800 p-1 flex items-center space-x-14 items-end">
     <NuxtLink to="/">
       <div class="container w-20 h-20">
         <Image class="w-auto h-auto" src="/img/logo.png" />
@@ -7,9 +8,31 @@
     </NuxtLink>
     <SearchBar />
     <Button
-      label="login"
+      v-for="link in links"
+      :key="link.name"
+      :label="link.name"
       link
       unstyled
       class="text-white hover:text-yellow-400 hover:cursor-pointer hover:underline" />
   </nav>
 </template>
+<script setup lang="ts">
+  const links = ref([
+    {
+      name: "Category",
+      icon: "pi pi-th-large",
+    },
+    {
+      name: "New Realeses",
+      icon: "pi pi-calendar",
+    },
+    {
+      name: "Popular",
+      icon: "pi pi-star",
+    },
+    {
+      name: "Blog",
+      icon: "pi pi-info-circle",
+    },
+  ]);
+</script>
